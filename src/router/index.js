8fdac807b34d1dd51d5cd3ff6d1de3cf64145b66
shortnd <ococncol@gmail.com>
2018-09-09 12:01:59 -0400

    added add movie page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/Home';
 import Contact from '@/components/Contact';
+import AddMovie from '@/components/AddMovie';
 
 Vue.use(Router);
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/contact',
       name: 'Contact',
       component: Contact,
+    },
+    {
+      path: '/movies/add',
+      name: 'AddMovie',
+      component: AddMovie,
     },
   ],
 });
